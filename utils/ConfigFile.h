ec67f123578ae7d18902f756d9dd54469e9d45c9
puhuaqiang <puhuaqiang@126.com>
2018-10-19 14:46:21 +0800

    update

--- utils/ConfigFile.h
+++ utils/ConfigFile.h
@@ -6,48 +6,37 @@
 namespace UTILS
 {
 	/**
-	\brief
-		ConfigFile class.
-		操作配置文件. Window(INI)
+	\brief 操作配置文件. Window(INI)
 	*/
 	class UTILS_API ConfigFile
 	{
 	public:
 		/**
-		\brief
-			构造函数,
+		\brief 构造函数,
 		*/
 		ConfigFile();
 		/**
-		\brief
-			构造函数
-		\param path
-			文件名,全路径
+		\brief 构造函数
+		\param path 文件名,全路径
 		*/
 		ConfigFile(const char* path);
 		~ConfigFile();
 
 		/**
-		\brief
-			读取整个配置文件到内存中.
-		\return
-			Error.h
+		\brief 读取整个配置文件到内存中.
+		\return Error.h
 		*/
 		void SetConfigFile(const char* path);
 
 		/**
-		\brief
-			读取整个配置文件到内存中.
-		\return
-			Error.h
+		\brief 读取整个配置文件到内存中.
+		\return Error.h
 		*/
 		const char* GetConfigFile() const { return szConfigFile; };
 
 		/**
-		\brief
-			读取整个配置文件到内存中.
-		\return
-			Error.h
+		\brief 读取整个配置文件到内存中.
+		\return Error.h
 		*/
 		static int ReadData(const char* lpszFileName,
 			std::list<std::map<std::string, std::list<std::pair<std::string, std::string>>>>& lstContent);
@@ -55,14 +44,10 @@ namespace UTILS
 			return UTILS::ConfigFile::ReadData(this->GetConfigFile(), lstContent);
 		}
 		/**
-		\brief
-			读取字符串.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 读取字符串.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int ReadStr(const char* lpszFileName, 
 			const char* lpszSection,
@@ -79,14 +64,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			读取整型值.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 读取整型值.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int ReadInt(const char* lpszFileName, 
 			const char* lpszSection, 
@@ -101,14 +82,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			写整型值.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 写整型值.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int WriteStr(const char* lpszFileName, 
 			const char* lpszSection, 
@@ -121,14 +98,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			写整型值.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 写整型值.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int WriteInt(const char* lpszFileName, 
 			const char* lpszSection, 
@@ -141,14 +114,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			删除关键字.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 删除关键字.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int DeleteKey(const char* lpszFileName, 
 			const char* lpszSection, 
@@ -159,14 +128,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			删除小节.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 删除小节.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int DeleteNode(const char* lpszFileName, 
 			const char* lpszSection);
@@ -175,14 +140,10 @@ namespace UTILS
 		}
 
 		/**
-		\brief
-			删除小节.
-		\param lpszFileName
-			配置文件全路径.
-		\param lpszSection
-			小节.
-		\return
-			存在返回TRUE，否则FALSE
+		\brief 删除小节.
+		\param lpszFileName 配置文件全路径.
+		\param lpszSection 小节.
+		\return 存在返回TRUE，否则FALSE
 		*/
 		static int Clear(const char* lpszFileName);
 		int Clear(){
